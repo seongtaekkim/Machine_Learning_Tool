7e9e1ff701af3082a4bc328ec7c9f415066be43e
unknown <chxortnl@gmail.com>
2020-01-10 06:57:41 +0900

    new file:   regressionEx/LinearRegressionEx.java

---
+++ src/main/java/machinelearning/regressionEx/LinearRegressionEx.java
@@ -0,0 +1,27 @@
+package machinelearning.regressionEx;
+
+import java.util.List;
+
+import machinelearning.regression.LinearRegression;
+
+public class LinearRegressionEx implements LinearRegression {
+
+	@Override
+	public void hypothesis() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Long costFuntion() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void gradientDecent() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
